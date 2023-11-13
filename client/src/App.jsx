73c920d73124ac0eb69989419275b9b8ff520ea2
admirerbrown <admirerbrown@gmail.com>
2023-11-13 20:16:39 +0000

    setup project with vite and react

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import NavBar from './Components/navBar'
+
+function App() {
+  return (
+    <>
+      <NavBar></NavBar>
+    </>
+  )
+}
+
+export default App
---
+++ client/src/Components/navBar.jsx
@@ -0,0 +1,10 @@
+const NavBar = ()=>{
+    return(
+        <div>
+        <h1 className='bg-red-500 text-blue-200 text-3xl text-center'>Great news here from New Delhi</h1>
+      </div>
+      
+    )
+}
+
+export default NavBar;
