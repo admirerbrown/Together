08e86df91ffcad17c289c66e4de39911e871ff6d
admirerbrown <admirerbrown@gmail.com>
2023-11-20 22:03:12 +0000

    mission section complete responsive

--- client/src/Components/homepage.jsx
+++ client/src/Components/homepage.jsx
@@ -1,9 +1,11 @@
 import HeroSection from "./hero"
+import Missions from "./mission"
 
 const HomePage =()=>{
   return (
     <>
       <HeroSection></HeroSection>
+      <Missions></Missions>
     </>
   )
 }
