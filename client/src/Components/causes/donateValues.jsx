983bef0bc2d4fbb453e24fdeaa0430ce18f9e169
admirerbrown <admirerbrown@gmail.com>
2024-02-05 18:47:22 +0000

    fix form logic and add toast notification

--- client/src/Components/causes/donateValues.jsx
+++ client/src/Components/causes/donateValues.jsx
@@ -1,6 +1,8 @@
 /* eslint-disable react/prop-types */
 import { useState, useRef, useEffect } from "react";
 import InfoForm from "./personalInfo";
+// import { toast } from "react-toastify";
+// import "react-toastify/dist/ReactToastify.css";
 
 const DonateForm = ({ item }) => {
   const donationOptions = [
@@ -19,7 +21,8 @@ const DonateForm = ({ item }) => {
 
   if (
     document.activeElement.tagName === "INPUT" &&
-    document.activeElement.id === "isfocused" && !isChecked
+    document.activeElement.id === "isfocused" &&
+    !isChecked
   ) {
     setSelectedAmount(" ");
     setIsChecked(true);
@@ -59,21 +62,26 @@ const DonateForm = ({ item }) => {
 
   const handleChange = () => {};
 
+
   return (
     <div className="font-Open-Sans mt-12 lg:mt-8 md:w-full">
       <h1 className="ml-10 text-[40px] leading-[2.7rem] pr-5 lg:ml-0 font-bold text-[#84c54e]">
         {item.title}
       </h1>
-      <p className="px-10 text-[12.5px] pt-8 lg:px-0 lg:pr-8 lg:text-[14px] xl:text-[15.5px] xl:w-[95%] leading-[1.5rem]">{item.description}</p>
-      
+      <p className="px-10 text-[12.5px] pt-8 lg:px-0 lg:pr-8 lg:text-[14px] xl:text-[15.5px] xl:w-[95%] leading-[1.5rem]">
+        {item.description}
+      </p>
+
       <div className="flex flex-col px-10 mb-10 lg:px-0">
-        <p className="text-[#84c54e] xl:text-[24px] font-bold mb-3 mt-5">Donation Form</p>
+        <p className="text-[#84c54e] xl:text-[24px] font-bold mb-3 mt-5">
+          Donation Form
+        </p>
         <div className="flex items-center gap-3">
           <label htmlFor="amount" className="text-[#8a8a8a] mb-2 text-[18px]">
             $
           </label>
           <input
-            type="text"
+            type="number"
             id="isfocused"
             onChange={handleChange}
             className="border h-9 w-[120px] input mt-2 mb-4 placeholder:text-xs focus:border-l-2 focus:border-r-0 focus:border-b-0 focus:border-t-2 focus:border-solid focus:outline-none focus:ring-transparent md:placeholder:text-sm xl:placeholder:text-lg rounded-[0.35rem] bg-[#eee] border-none"
